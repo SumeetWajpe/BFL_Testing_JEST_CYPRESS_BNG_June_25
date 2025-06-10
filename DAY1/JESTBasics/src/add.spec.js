2899746b8d431869a3e6e47933ee014956770704
Sumeet Wajpe <sumeet.wajpe@hotmail.com>
2025-06-10 10:52:39 +0530

    ✨ using describe & it

--- DAY1/JESTBasics/src/add.spec.js
+++ DAY1/JESTBasics/src/add.spec.js
@@ -1,11 +1,23 @@
 const Add = require("./add");
+// using JEST test method
+// test("adds 10 + 20 to equal 30", () => {
+//   // Arrange
+//   let result;
+//   // Act
+//   result = Add(10, 20);
+//   // Assert
 
-test("adds 10 + 20 to equal 30", () => {
-  // Arrange
-  let result;
-  // Act
-  result = Add(10, 20);
-  // Assert
+//   expect(result).toBe(30);
+// });
 
-  expect(result).toBe(30);
+// Test suite & test case
+describe("test suite for addition", () => {
+  it("adds two numbers", () => {
+    // Arrange
+    let result;
+    // Act
+    result = Add(10, 20);
+    // Assert
+    expect(result).toBe(30);
+  });
 });
