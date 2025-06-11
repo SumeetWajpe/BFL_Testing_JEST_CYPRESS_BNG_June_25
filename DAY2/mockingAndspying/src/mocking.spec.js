74e1c524c610f73b670cbd126176fa62d6a104f7
Sumeet Wajpe <sumeet.wajpe@hotmail.com>
2025-06-11 09:51:14 +0530

    ✨ Creating and executing mocks

---
+++ DAY2/mockingAndspying/src/mocking.spec.js
@@ -0,0 +1,19 @@
+function forEach(items, callback) {
+  for (const item of items) {
+    callback(item);
+  }
+}
+
+describe("test suite for mocking", () => {
+  it("for mock function to be called", () => {
+    const mockCallback = jest.fn(x => {
+      console.log("Mocked Callback called !");
+      return 20 + x;
+    });
+
+    forEach([10, 20], mockCallback);
+    // expect(mockCallback.mock.calls.length).toBe(2);
+    // expect(mockCallback.mock.calls[0][0]).toBe(10);
+    // expect(mockCallback.mock.calls[1][0]).toBe(20);
+  });
+});
