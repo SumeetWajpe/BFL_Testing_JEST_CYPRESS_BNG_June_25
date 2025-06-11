2936ef023ad12ae8374b0004f0dc0c684c249b16
Sumeet Wajpe <sumeet.wajpe@hotmail.com>
2025-06-11 15:40:09 +0530

    ✨ using fixture.debugElement.injector.get()

--- DAY2/pokedex_app/src/app/components/pokemon-list/pokemon-list.component.spec.ts
+++ DAY2/pokedex_app/src/app/components/pokemon-list/pokemon-list.component.spec.ts
@@ -1,35 +1,47 @@
-import { TestBed } from "@angular/core/testing";
+import { ComponentFixture, TestBed } from "@angular/core/testing";
 import { PokemonListComponent } from "./pokemon-list.component";
 import { PokemonItemComponent } from "../pokemon-item/pokemon-item.component";
-import { FilterPokemonPipe } from "src/app/shared/pipes/filter-pokemon.pipe";
-import { HeaderComponent } from "src/app/shared/components/header/header.component";
-import { StatsFilterComponent } from "src/app/shared/components/stats-filter/stats-filter.component";
-import { DialogComponent } from "src/app/shared/components/dialog/dialog.component";
+import { FilterPokemonPipe } from "../../shared/pipes/filter-pokemon.pipe";
+import { HeaderComponent } from "../../shared/components/header/header.component";
+import { StatsFilterComponent } from "../../shared/components/stats-filter/stats-filter.component";
+import { DialogComponent } from "../../shared/components/dialog/dialog.component";
 import { PokemonDetailsComponent } from "../pokemon-details/pokemon-details.component";
-import { PokemonService } from "src/app/shared/services/pokemon.service";
-import { HttpClientTestingModule } from "@angular/common/http/testing";
+import { PokemonService } from "../../shared/services/pokemon.service";
+import {
+  HttpClientTestingModule,
+  HttpTestingController,
+} from "@angular/common/http/testing";
 import { FormsModule } from "@angular/forms";
 import { NgMultiSelectDropDownModule } from "ng-multiselect-dropdown";
 
 describe("test suite for PokemonList Component", () => {
-    beforeEach(async()=>{
-        await TestBed.configureTestingModule({
-          declarations: [
-            PokemonListComponent,
-            PokemonItemComponent,
-            FilterPokemonPipe,
-            HeaderComponent,
-            StatsFilterComponent,
-            DialogComponent,
-            PokemonDetailsComponent,
-          ],
-          providers: [PokemonService],
-          imports: [
-            HttpClientTestingModule,
-            FormsModule,
-            NgMultiSelectDropDownModule,
-          ],
-        }).compileComponents(); 
-    });
-  it("", () => {});
+  let compInstance: PokemonListComponent;
+  let fixture: ComponentFixture<PokemonListComponent>;
+  let httpMock: HttpTestingController;
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [
+        PokemonListComponent,
+        PokemonItemComponent,
+        FilterPokemonPipe,
+        HeaderComponent,
+        StatsFilterComponent,
+        DialogComponent,
+        PokemonDetailsComponent,
+      ],
+      providers: [PokemonService],
+      imports: [
+        HttpClientTestingModule,
+        FormsModule,
+        NgMultiSelectDropDownModule,
+      ],
+    }).compileComponents();
+    httpMock = TestBed.inject(HttpTestingController);
+    fixture = TestBed.createComponent(PokemonListComponent);
+    compInstance = fixture.componentInstance;
+  });
+  it("tess if DI framework creates the service instance & its dependencies", () => {
+    let servInstance = fixture.debugElement.injector.get(PokemonService);
+    expect(servInstance).toBeTruthy();
+  });
 });
