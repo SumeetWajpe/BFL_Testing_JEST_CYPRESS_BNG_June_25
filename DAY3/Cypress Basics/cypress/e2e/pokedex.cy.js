af39ada55d611d7bed77bd4f0937626c357f45c4
Sumeet Wajpe <sumeet.wajpe@hotmail.com>
2025-06-12 12:48:27 +0530

    ✨ Getting text of span.main-title & asserting

--- DAY3/Cypress Basics/cypress/e2e/pokedex.cy.js
+++ DAY3/Cypress Basics/cypress/e2e/pokedex.cy.js
@@ -1,5 +1,16 @@
+// /// <reference types="Cypress" />
 describe("test suite for pokedex app", () => {
   it("should visit pokedex app at localhost", () => {
     cy.visit("http://localhost:4200/");
+    //cy.get("#search_pokemon").type("bu");
+    //cy.get(".pokemon-content").should("have.length", 2);
+    // cy.get("app-pokemon-item")
+    //   .find(".pokemon-content")
+    //   .should("have.length", 2);
+
+    cy.get("span.main-title").should(
+      "have.text",
+      " Search for any Pokemon that exists on the planet ",
+    );
   });
 });
