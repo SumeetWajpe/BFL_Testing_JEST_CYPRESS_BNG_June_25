ef9533b7c8b48c8215fe2478014a7c939120b664
Sumeet Wajpe <sumeet.wajpe@hotmail.com>
2025-06-12 14:22:33 +0530

    ✨ Handling click of the first pokemon in the list

--- DAY3/Cypress Basics/cypress/e2e/pokedex.cy.js
+++ DAY3/Cypress Basics/cypress/e2e/pokedex.cy.js
@@ -2,7 +2,7 @@
 describe("test suite for pokedex app", () => {
   it("should visit pokedex app at localhost", () => {
     cy.visit("http://localhost:4200/");
-    //cy.get("#search_pokemon").type("bu");
+    cy.get("#search_pokemon").type("bu");
     //cy.get(".pokemon-content").should("have.length", 2);
     // cy.get("app-pokemon-item")
     //   .find(".pokemon-content")
@@ -13,6 +13,7 @@ describe("test suite for pokedex app", () => {
     //   " Search for any Pokemon that exists on the planet ",
     // );
 
-   // cy.get("span.main-title").text().should("contain", "Search for any Pokemon that exists on the planet");
+    // cy.get("span.main-title").text().should("contain", "Search for any Pokemon that exists on the planet");
+    cy.get("app-pokemon-item").find(".pokemon-content").first().click();
   });
 });
